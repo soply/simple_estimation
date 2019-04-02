2094c86e584d9367fcb13e4feb6596e471f4dce3
Timo Klock <timo@Timos-MBP.fritz.box>
2019-04-02 09:34:06 +0200

    Added directional regression + kNN regression

---
+++ estimators/DRKnn.py
@@ -0,0 +1,58 @@
+# coding: utf8
+import inspect
+import os
+import sys
+
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsRegressor
+
+# Full path
+sys.path.insert(0, '../../sdr_toolbox/')
+
+from sdr_toolbox.sdr_estimators.directional_regression import directional_regression
+
+
+class DRKnn(BaseEstimator, RegressorMixin):
+    """
+    Implementing SIR for dimension reduction + kNN on reduced features as
+    regressor.
+    """
+
+    def __init__(self, n_neighbors = 1, n_components = 1,
+                 n_levelsets = 1, n_jobs = 1, rescale = True):
+        """
+
+        """
+        # Set attributes of object to the same name as given in the argument
+        # list.
+        args, _, _, values = inspect.getargvalues(inspect.currentframe())
+        values.pop("self")
+
+        for arg, val in values.items():
+            setattr(self, arg, val)
+
+
+    def fit(self, X, y=None):
+        """
+        """
+        if self.n_components > self.n_levelsets:
+            raise RuntimeError("n_components = {0} > {1} = n_levelsets".format(self.n_components, self.n_levelsets))
+        n_samples, n_features = X.shape
+        self.DR_space_ = directional_regression(X.T, y, d = self.n_components,
+                                              n_levelsets = self.n_levelsets,
+                                              rescale = self.rescale)
+        self.XT_ = (self.DR_space_.T.dot(X.T)).T
+        self.knn_ = KNeighborsRegressor(n_neighbors = self.n_neighbors,
+                                        n_jobs = self.n_jobs)
+        self.knn_ = self.knn_.fit(self.XT_, y)
+        return self
+
+
+    def predict(self, X, y=None):
+        try:
+            getattr(self, "knn_")
+        except AttributeError:
+            raise RuntimeError("You must train estimator before predicting data!")
+
+        return self.knn_.predict(self.DR_space_.T.dot(X.T).T)
